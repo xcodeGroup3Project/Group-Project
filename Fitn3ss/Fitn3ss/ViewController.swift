419929b0646e64e1c84947fe8bfd9fa051ccd728
Ethan Mendoza <83525058+lsdbf@users.noreply.github.com>
2023-04-02 01:47:48 -0400

    finished meal tab

--- Fitn3ss/Fitn3ss/ViewController.swift
+++ Fitn3ss/Fitn3ss/ViewController.swift
@@ -12,11 +12,56 @@ class ViewController: UIViewController {
     @IBOutlet var BreakfastTableView: UITableView!
     @IBOutlet var LunchTableView: UITableView!
     @IBOutlet var DinnerTableView: UITableView!
+    
+    
+    @IBOutlet var titlelabel: UILabel!
+    
+    var models: [(title: String, calories_count: String)] = [ ]
+    var modelsLunch: [(title: String, calories_count: String)] = [ ]
+    var modelsDinner: [(title: String, calories_count: String)] = [ ]
+    
     @IBAction func newMeal(){
         
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "breakfast") as? EntryViewController else {
+            return
+        }
+        vc.title = "New Meal"
+        vc.completion = {MealName, calories in
+            self.navigationController?.popToRootViewController(animated: true)
+            self.models.append((title: MealName, calories_count: calories))
+            self.BreakfastTableView.reloadData()
+        }
+        navigationController?.pushViewController(vc, animated: true)
     }
-    
-    var models : [(title: String, calories: String)] = []
+    //------------------------------------------------------------------------------------------------------------------
+    @IBAction func newLunch(){
+        
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "lunch") as? EntryViewController else {
+            return
+        }
+        vc.title = "New Meal"
+        vc.completion = {MealName, calories in
+            self.navigationController?.popToRootViewController(animated: true)
+            self.modelsLunch.append((title: MealName, calories_count: calories))
+            self.LunchTableView.reloadData()
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    //------------------------------------------------------------------------------------------------------------------
+    @IBAction func newDinner(){
+        
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "dinner") as? EntryViewController else {
+            return
+        }
+        vc.title = "New Meal"
+        vc.completion = {MealName, calories in
+            self.navigationController?.popToRootViewController(animated: true)
+            self.modelsDinner.append((title: MealName, calories_count: calories))
+            self.DinnerTableView.reloadData()
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    //------------------------------------------------------------------------------------------------------------------
     let test_values1 = ["222","222","222"]
     let test_values2 = ["333","333","333"]
     let test_values3 = ["444","444","444"]
@@ -42,13 +87,13 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource
 {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if tableView.tag == 0 {
-            return test_values1.count
+            return models.count
         }
         else if tableView.tag == 1 {
-            return test_values2.count
+            return models.count
         }
         else {
-            return test_values3.count
+            return models.count
         }
         
     }
@@ -57,28 +102,33 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource
         tableView.deselectRow(at: indexPath, animated: true)
         //show the meal controller
         
-        guard let vc = storyboard?.instantiateViewController(withIdentifier: "meal") as? MealViewController else {
+        print("selected row")
+        /*guard let vc = storyboard?.instantiateViewController(withIdentifier: "meal") as? MealViewController else {
                 return
         }
         vc.title = "Meal"
-        navigationController?.pushViewController(vc, animated: true)
+        navigationController?.pushViewController(vc, animated: true)*/
             
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if tableView.tag == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "Breakfast", for: indexPath)
-            cell.textLabel?.text = test_values1[indexPath.row]
+            cell.textLabel?.text = models[indexPath.row].title
+            cell.detailTextLabel?.text = models[indexPath.row].calories_count
             return cell
         }
         else if tableView.tag == 1 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "Lunch", for: indexPath)
-            cell.textLabel?.text = test_values2[indexPath.row]
+            cell.textLabel?.text = modelsLunch[indexPath.row].title
+            cell.detailTextLabel?.text = modelsLunch[indexPath.row].calories_count
             return cell
         }
-        else {
+        else  {
             let cell = tableView.dequeueReusableCell(withIdentifier: "Dinner", for: indexPath)
-            cell.textLabel?.text = test_values3[indexPath.row]
+            cell.textLabel?.text = modelsDinner[indexPath.row].title
+            cell.detailTextLabel?.text = modelsDinner[indexPath.row].calories_count
             return cell
         }
+        
     }
 }
